b84d831a41f1e75a641ab0bd58157c0e8edc436a
Gokulkrishna14 <nairgokul22@gmail.com>
2024-06-16 15:53:01 +0530

    Todo Project Completed using useCallback

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -2,17 +2,18 @@ import './App.css';
 import TodoForm from './component/TodoForm/TodoForm';
 import {TodoProvider} from "./context" 
 import { useEffect, useState } from 'react';
+import TodoItem from "./component/TodoItem/TodoItem"
 
 function App() {
 
   const[todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
-    setTodos((prevTodos) => [{id: Date.now(), ...todo}, ...prevTodos]);
+    setTodos((prevTodos) => [{ id: Date.now(), ...todo }, ...prevTodos]);
   };
 
   const editTodo = (id, todo) => {
-    setTodos((prevTodos) => prevTodos.map(prevTodo => (prevTodo.id === id) ? prevTodo : todo))
+    setTodos((prevTodos) => prevTodos.map(prevTodo => (prevTodo.id === id) ? todo : prevTodo))
   };
 
   const deleteTodo = (id) => {
@@ -31,14 +32,23 @@ function App() {
   },[]);
 
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem("Todos", JSON.stringify(todos));
   }, [todos]);
+
 
   return (
     <TodoProvider value={{todos, addTodo, editTodo, deleteTodo, completeTodo}}>
       <div className='container'>
         <h1>Manage Your Todos</h1>
         <TodoForm />
+        {
+          todos.map(todo => (
+            <div className='item' key={todo.id}>
+              <TodoItem todo={todo} />
+            </div>
+          ))
+        }
+        
       </div>
     </TodoProvider>
   );
--- todoapp/src/component/TodoItem/TodoItem.jsx
+++ todoapp/src/component/TodoItem/TodoItem.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import styles from "./TodoItem.module.css";
+import SaveIcon from '@mui/icons-material/Save';
+import {useTodo} from "../../context/index"
 
-export default function TodoItem() {
+export default function TodoItem({todo}) {
+  const [isEdititable, setIsEdititable] = useState(false);
+  const [todoMsg, setTodoMsg] = useState(todo.todo);
+  const {editTodo, deleteTodo, completeTodo} = useTodo();
+
+  const edit = () => {
+    editTodo(todo.id, {...todo, todo: todoMsg});
+  }
+
   return (
-    <div>TodoItem</div>
+    <div className={styles.containerItem}>
+      <div className={styles.checkboxContainer}>
+        <input 
+        type="checkbox" 
+        className={styles.checkbox}
+        checked={todo.completed}
+        onChange={() => completeTodo(todo.id)}
+        />
+      </div>
+      <input 
+      type="text" 
+      value={todoMsg} 
+      className={`${styles.input} ${isEdititable ? styles.noneditField : styles.editField} ${todo.completed && styles.strikeout}`}
+      readOnly={!isEdititable}
+      onChange={(e) => setTodoMsg(e.target.value)}
+      />
+      <button 
+      className={`${styles.button} ${styles.edit} ${todo.completed ? styles.disabled : styles.enabled}`
+      }
+      onClick={() => {
+        if(todo.completed) return;
+        if(isEdititable){
+          edit(todo.id)
+        }setIsEdititable((prev) => !prev)
+      }}
+      disabled={todo.completed}
+      >
+        {isEdititable ? <SaveIcon /> : <EditIcon />}
+      </button>
+      <button 
+      className={`${styles.button} ${styles.delete}`}
+      onClick={() => deleteTodo(todo.id)}
+      >
+        <DeleteIcon />
+      </button>
+    </div>
   );
 }
